21d96c02bb63afa23bb383a42bafe3006f9a002b
renanranzani <renanranzani@gmail.com>
2021-09-01 14:55:45 -0300

    inicio do app

---
+++ ConfiguraImpressora/Interface/IEAPrinterManager.cs
@@ -0,0 +1,13 @@
+﻿using ConfiguraImpressora.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfiguraImpressora.Interface
+{
+    internal interface IEAPrinterManager
+    {
+        bool ConfiguraImpressora(PrinterModel impressora);
+    }
+}
---
+++ ConfiguraImpressora/Model/PrinterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ConfiguraImpressora.Model
+{
+    internal class PrinterModel
+    {
+        public string Name { get; set; }
+        public string DriverName { get; set; }
+        public string ServerName { get; set; }
+        public string PortName { get; set; }
+        public string Default { get; set; }
+        public string Familia { get; set; }
+        public string Tipo { get; set; }
+        public string Ip { get; set; }
+    }
+}
